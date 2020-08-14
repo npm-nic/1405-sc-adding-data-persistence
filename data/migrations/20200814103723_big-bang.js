f6499829f0e21b70921e69690e69506bdcf42617
Nic <53277092+npm-nic@users.noreply.github.com>
2020-08-14 10:59:39 -0500

    🚀 big-bang -- v1  db

---
+++ data/migrations/20200814103723_big-bang.js
@@ -0,0 +1,34 @@
+exports.up = function (knex) {
+  return knex.schema
+    .createTable("projects", (tbl) => {
+      tbl.increments();
+      tbl.string("name").notNullable();
+      tbl.string("description");
+      tbl.boolean("completed").default(false);
+    })
+    .createTable("tasks", (tbl) => {
+      tbl.increments();
+      tbl.string("description").notNullable();
+      tbl.string("notes");
+      tbl.boolean("completed").default(false);
+      tbl.integer("project_id").unsigned().references("projects.id");
+    })
+    .createTable("resources", (tbl) => {
+      tbl.increments();
+      tbl.string("name").unique().notNullable();
+      tbl.string("description");
+    })
+    .createTable("project_resources", (tbl) => {
+      tbl.increments();
+      tbl.integer("project_id").unsigned().references("projects.id");
+      tbl.integer("resource_ud").unsigned().references("resources.id");
+    });
+};
+
+exports.down = function (knex) {
+  return knex.schema
+    .dropTableIfExists("projects")
+    .dropTableIfExists("tasks")
+    .dropTableIfExists("resources")
+    .dropTableIfExists("project_resources");
+};
